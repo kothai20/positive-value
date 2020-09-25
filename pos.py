5de19dbb99c67c664ab5171564fa29daa130a308
kothai20 <71602542+kothai20@users.noreply.github.com>
2020-09-25 19:32:41 +0530

    Add files via upload

---
+++ pos.py
@@ -0,0 +1,14 @@
+list1=[]
+n=int(input("enter a values"))
+for i in  range (0,n,1):
+    n=int(input())
+    list1.append(n)
+print(list1)
+for a in list1:
+    if a>0:print(a)
+
+    
+
+    
+
+        
